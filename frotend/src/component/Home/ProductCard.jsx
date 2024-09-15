4814bac80f5efad355b37ded75e68f16ad96ea6b
Pratip Kayal <pratipkayal1@gmail.com>
2024-09-15 13:32:08 +0530

    commit

--- frotend/src/component/Home/ProductCard.jsx
+++ frotend/src/component/Home/ProductCard.jsx
@@ -1,192 +1,31 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-  Button,
-  Box,
-} from "@material-ui/core";
-import Rating from "@material-ui/lab/Rating";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import {
-  dispalyMoney,
-  generateDiscountedPrice,
-} from "../DisplayMoney/DisplayMoney";
+import { MDBCard, MDBCardBody, MDBCardImage, MDBRipple } from "mdb-react-ui-kit";
+import { Rating, Typography } from "@mui/material";
 import { addItemToCart } from "../../actions/cartAction";
-
 import placeholderImage from "../../Image/pdf.png";
+import { dispalyMoney, generateDiscountedPrice } from "../DisplayMoney/DisplayMoney";
+import "./ProductCard.css";
+import { FaShoppingCart } from "react-icons/fa"; // FontAwesome shopping cart icon
 
-import exclusiveIcon from "../../Image/stars.png";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "300px",
-    height: "fit-content",
-
-    backgroundColor: "#ffffff",
-    border: "1px solid #E3E3E3",
-    borderRadius: "12px",
-    cursor: "pointer",
-
-    transition: "transform 0.3s, box-shadow 0.3s, border 0.3s",
-    position: "relative",
-
-    [theme.breakpoints.down(767)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(721)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(600)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(540)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(480)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(440)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(420)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(412)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(393)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(360)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-    [theme.breakpoints.down(320)]: {
-      width: "90%",
-      margin: theme.spacing(1),
-    },
-  },
-  media: {
-    height: "190px",
-    width: "100%",
-    borderTopLeftRadius: "12px",
-    borderTopRightRadius: "12px",
-    overflow: "hidden",
-    position: "relative",
-  },
-  mediaImg: {
-    width: "100%",
-    height: "100%",
-    objectFit: "contain",
-    marginTop:"10px"
-  },
-  button: {
-    marginTop: -10,
-    backgroundColor: "#007BFF",
-    color: "white",
-    transition: "background-color 0.3s",
-    borderRadius: "0 0 12px 12px",
-    fontFamily: "'Outfit', sans-serif",
-    fontWeight: "bold",
-    width: "100%",
-    height: 45,
-    "&:hover": {
-      backgroundColor: "#0056b3",
-    },
-  },
-  oldPrice: {
-    textDecoration: "line-through",
-    fontFamily: "'Outfit', sans-serif",
-    fontWeight: "normal",
-    borderRadius: "5px",
-    padding: "0 8px",
-    fontSize: "var(--font-span)",
-    backgroundColor: "#ff4e00",
-    color: "#fff",
-    marginRight: theme.spacing(1),
-   
-  },
-  finalPrice: {
-    color: "#fff",
-    backgroundColor: "#28a745",
-    fontFamily: "'Outfit', sans-serif",
-    borderRadius: "5px",
-    padding: "0 8px",
-    fontWeight: "normal",
-    fontSize: "var(--font-span)",
-   
-  },
-  description: {
-    fontSize: "var(--font-p)",
-    fontFamily: "'Outfit', sans-serif",
-    fontWeight: 500,
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-    display: "-webkit-box",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    WebkitLineClamp: 3,
-    WebkitBoxOrient: "vertical",
-  },
-  title: {
-    fontWeight: "700",
-    fontFamily: "'Outfit', sans-serif",
-    fontSize: "var(--font-p)",
-  },
-  exclusiveIcon: {
-    position: "absolute",
-    top: "8px",
-    left: "8px",
-    width: "50px",
-    height: "50px",
-    backgroundImage: `url(${exclusiveIcon})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    zIndex: 1,
-  },
-}));
-
-const ProductCard = ({ product }) => {
-  React.useEffect(() => {
+function ProductCard({ product }) {
+  useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
 
   const dispatch = useDispatch();
-  const classes = useStyles();
 
   if (!product) {
     return (
-      <Card className={classes.root} elevation={6}>
-        <CardContent>
-          <Typography variant="body1">Loading...</Typography>
-          <CardMedia className={classes.media}>
-            <img
-              className={classes.mediaImg}
-              src={placeholderImage}
-              alt="Placeholder"
-            />
-          </CardMedia>
-          <CardContent>
-            <Typography variant="body2">Product details loading..</Typography>
-          </CardContent>
-        </CardContent>
-      </Card>
+      <MDBCard className="product-card">
+        <MDBCardBody className="d-flex flex-column">
+          <h5 className="card-title">Loading...</h5>
+          <MDBCardImage src={placeholderImage} alt="Placeholder" fluid className="card-img-top" />
+          <p>Product details loading...</p>
+        </MDBCardBody>
+      </MDBCard>
     );
   }
 
@@ -201,95 +40,65 @@ const ProductCard = ({ product }) => {
   discountPrice = dispalyMoney(discountPrice);
   const oldPrice = dispalyMoney(product.price);
 
-  const truncated =
-    product.description.split(" ").slice(0, 6).join(" ") + "...";
-  const nameTruncated = product.name.split(" ").slice(0, 6).join(" ") + "...";
+  const truncatedDescription = product.description.split(" ").slice(0, 6).join(" ") + "...";
 
-  const addTocartHandler = (id, qty) => {
+  const addToCartHandler = (id, qty) => {
     dispatch(addItemToCart(id, qty));
   };
 
   return (
-    <Card className={classes.root} elevation={6}>
-      <div className={classes.exclusiveIcon} />
-      <Link
-        className="productCard"
-        to={`/product/${product._id}`}
-        style={{ textDecoration: "none", color: "inherit" }}
-      >
-        <CardActionArea>
-          <CardMedia className={classes.media}>
-            <img
-              className={classes.mediaImg}
-              src={imageUrl}
-              alt={product.name}
-            />
-          </CardMedia>
-          <CardContent>
-            <Typography gutterBottom color="black" className={classes.title}>
-              {nameTruncated}
-            </Typography>
-
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="div"
-              className={classes.description}
-            >
-              {truncated}
-            </Typography>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <Box display="flex" alignItems="center" marginTop="1rem">
-                <Typography variant="body1" className={classes.oldPrice}>
-                  {oldPrice}
-                </Typography>
-                <Typography variant="body1" className={classes.finalPrice}>
-                  {discountPrice}
-                </Typography>
-              </Box>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-around",
-                  marginTop: "10px",
-                }}
-              >
-                <Rating
-                  name="rating"
-                  value={product.ratings}
-                  precision={0.1}
-                  readOnly
-                  size="small"
-                  style={{
-                    color: "#ed1c24",
-                    marginRight: 8,
-                    fontWeight: "400",
-                  }}
-                />
-                <Typography variant="body2" color="textSecondary">
-                  ({product.numOfReviews})
-                </Typography>
-              </div>
-            </Box>
-          </CardContent>
-        </CardActionArea>
-      </Link>
-      <Box display="flex" justifyContent="center" p={1}>
-        <Button
-          variant="contained"
-          className={classes.button}
-          onClick={() => addTocartHandler(product._id, 1)}
-        >
-          Add to Cart
-        </Button>
-      </Box>
-    </Card>
+    <MDBCard className="product-card">
+      <MDBRipple rippleColor="light" rippleTag="div" className="bg-image rounded hover-zoom">
+        <MDBCardImage src={imageUrl} fluid className="card-img-top" />
+        <a href={`/product/${product._id}`}>
+          <div className="mask">
+            <div className="d-flex justify-content-start align-items-end h-100">
+              <h5>
+                <span className="badge bg-primary ms-2">New</span>
+                {product.isEco && <span className="badge bg-success ms-2">Eco</span>}
+                {product.discount > 0 && <span className="badge bg-danger ms-2">-{product.discount}%</span>}
+              </h5>
+            </div>
+          </div>
+        </a>
+      </MDBRipple>
+      <MDBCardBody className="d-flex flex-column">
+        <a href={`/product/${product._id}`} className="text-reset">
+          <h5 className="card-title mb-1">{product.name}</h5>
+        </a>
+        
+        {/* Rating and Reviews Section */}
+        <div className="d-flex align-items-center mb-1">
+          <Rating
+            name="rating"
+            value={product.ratings}
+            precision={0.1}
+            readOnly
+            size="small"
+            style={{
+              color: "#ed1c24",
+              marginRight: 8,
+              fontWeight: "400",
+            }}
+          />
+          <Typography variant="body2" color="textSecondary">
+            ({product.numOfReviews})
+          </Typography>
+        </div>
+        
+        <p>{truncatedDescription}</p> {/* Display the truncated description here */}
+        <div className="price-section mb-1">
+          <span className="old-price">{oldPrice}</span>
+          <span className="discount-price">{discountPrice}</span>
+        </div>
+        <div className="mt-1">
+          <button className="btn btn-primary" onClick={() => addToCartHandler(product._id, 1)}>
+            <FaShoppingCart className="icon" /> Add to Cart
+          </button>
+        </div>
+      </MDBCardBody>
+    </MDBCard>
   );
-};
+}
 
 export default ProductCard;
