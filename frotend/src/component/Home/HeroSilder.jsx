67c001057ec6d1b8b6cd6e896002fab39e10a383
Pratip Kayal <pratipkayal1@gmail.com>
2024-10-21 19:35:01 +0530

    commitnew2

--- frotend/src/component/Home/HeroSilder.jsx
+++ frotend/src/component/Home/HeroSilder.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 import Typed from "react-typed";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaChevronLeft, FaChevronRight, FaArrowRight } from "react-icons/fa"; // Import the arrow icon
-import slider1Webp from "../../Image/heroslider/BG.png";
-import slider2Webp from "../../Image/heroslider/BG1.png";
-import slider3Webp from "../../Image/heroslider/BG2.png";
+import slider1Webp from "../../Image/BG.png";
+import slider2Webp from "../../Image/BG1.png";
+import slider3Webp from "../../Image/BG2.png";
 import "./HeroSlider.css";
 
 const slides = [
